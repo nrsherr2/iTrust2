55583bb77276c0418945f80933c523b8d7768563
Nicholas Wrenn <nlwrenn@ncsu.edu>
2018-04-17 18:40:59 -0400

    Issue-#24: Fixed the definition of some cucumber steps in the EmergencyRecordsStepDefs file.

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/EmergencyRecordsStepDefs.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/EmergencyRecordsStepDefs.java
@@ -1,20 +1,11 @@
 package edu.ncsu.csc.itrust2.cucumber;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-import java.util.Locale;
 import java.util.Calendar;
-
-import edu.ncsu.csc.itrust2.models.persistent.Patient;
-import edu.ncsu.csc.itrust2.models.persistent.User;
-import edu.ncsu.csc.itrust2.models.enums.Role;
-import edu.ncsu.csc.itrust2.models.enums.State;
+import java.util.Locale;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -29,105 +20,111 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import edu.ncsu.csc.itrust2.models.enums.Role;
+import edu.ncsu.csc.itrust2.models.enums.State;
+import edu.ncsu.csc.itrust2.models.persistent.Patient;
+import edu.ncsu.csc.itrust2.models.persistent.User;
 
 public class EmergencyRecordsStepDefs {
 
-	int PATIENT_AGE = 100;
-	String PATIENT_NAME = "patient";
-	String PATIENT_DOB = "01/01/2011";
-	String PATIENT_GENDER = "Male";
-	String PATIENT_BLOOD = "APos";
+    int                  PATIENT_AGE    = 100;
+    String               PATIENT_NAME   = "patient";
+    String               PATIENT_DOB    = "01/01/2011";
+    String               PATIENT_GENDER = "Male";
+    String               PATIENT_BLOOD  = "APos";
 
-	private WebDriver driver;
-	private final String baseUrl = "http://localhost:8080/iTrust2";
+    private WebDriver    driver;
+    private final String baseUrl        = "http://localhost:8080/iTrust2";
 
-	WebDriverWait wait;
+    WebDriverWait        wait;
 
-	@Before
-	public void setup() {
+    @Before
+    public void setup () {
 
-		driver = new HtmlUnitDriver(true);
-		wait = new WebDriverWait(driver, 5);
+        driver = new HtmlUnitDriver( true );
+        wait = new WebDriverWait( driver, 5 );
 
-		final User hcp = new User("hcp", "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.", Role.ROLE_HCP, 1);
-		hcp.save();
+        final User hcp = new User( "hcp", "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.", Role.ROLE_HCP,
+                1 );
+        hcp.save();
 
-		final User er = new User("Emergency", "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.", Role.ROLE_HCP, 1);
-		er.save();
-	}
+        final User er = new User( "Emergency", "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.",
+                Role.ROLE_HCP, 1 );
+        er.save();
+    }
 
-	@After
-	public void tearDown() {
-		driver.close();
-	}
+    @After
+    public void tearDown () {
+        driver.close();
+    }
 
-	@Given("There is a patient with the name: (.+)")
-	public void patientExists(final String name) throws ParseException {
+    @Given ( "There is a patient with the name: (.+)" )
+    public void patientExists ( final String name ) throws ParseException {
 
-		/* Create patient record */
+        /* Create patient record */
 
-		final Patient patient = new Patient();
-		patient.setSelf(User.getByName(PATIENT_NAME));
-		patient.setFirstName(name.split(" ")[0]);
-		patient.setLastName(name.split(" ")[1]);
-		patient.setEmail("email@mail.com");
-		patient.setAddress1("847 address place");
-		patient.setCity("citytown");
-		patient.setState(State.CA);
-		patient.setZip("91505");
-		patient.setPhone("123-456-7890");
-		final SimpleDateFormat sdf = new SimpleDateFormat("MM/DD/YYYY", Locale.ENGLISH);
+        final Patient patient = new Patient();
+        patient.setSelf( User.getByName( PATIENT_NAME ) );
+        patient.setFirstName( name.split( " " )[0] );
+        patient.setLastName( name.split( " " )[1] );
+        patient.setEmail( "email@mail.com" );
+        patient.setAddress1( "847 address place" );
+        patient.setCity( "citytown" );
+        patient.setState( State.CA );
+        patient.setZip( "91505" );
+        patient.setPhone( "123-456-7890" );
+        final SimpleDateFormat sdf = new SimpleDateFormat( "MM/DD/YYYY", Locale.ENGLISH );
 
-		final Calendar time = Calendar.getInstance();
-		time.setTime(sdf.parse(PATIENT_DOB));
+        final Calendar time = Calendar.getInstance();
+        time.setTime( sdf.parse( PATIENT_DOB ) );
 
-		patient.setDateOfBirth(time);
+        patient.setDateOfBirth( time );
 
-		patient.save();
+        patient.save();
 
-	}
+    }
 
-	@When("I log into iTrust2 as an ER")
-	public void erLogin() {
-		driver.get(baseUrl);
-		final WebElement username = driver.findElement(By.name("username"));
-		username.clear();
-		username.sendKeys("Emergency");
-		final WebElement password = driver.findElement(By.name("password"));
-		password.clear();
-		password.sendKeys("123456");
-		final WebElement submit = driver.findElement(By.className("btn"));
-		submit.click();
-	}
+    @When ( "I log into iTrust2 as an ER" )
+    public void erLogin () {
+        driver.get( baseUrl );
+        final WebElement username = driver.findElement( By.name( "username" ) );
+        username.clear();
+        username.sendKeys( "Emergency" );
+        final WebElement password = driver.findElement( By.name( "password" ) );
+        password.clear();
+        password.sendKeys( "123456" );
+        final WebElement submit = driver.findElement( By.className( "btn" ) );
+        submit.click();
+    }
 
-	@When("I navigate to the Emergency Health Records Page")
-	public void navigateToOfficeVisit() {
-		((JavascriptExecutor) driver).executeScript("document.getElementById('emergencyHealthRecords').click();");
-	}
+    @When ( "I navigate to the Emergency Health Records Page" )
+    public void navigateToOfficeVisit () {
+        ( (JavascriptExecutor) driver ).executeScript( "document.getElementById('emergencyHealthRecords').click();" );
+    }
 
-	@When("When I fill in the username of the patient")
-	public void searchName() {
+    @When ( "I fill in the username of the patient" )
+    public void searchName () {
 
-		// Enter the name
-		String ename = "searchName";
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(ename)));
-		final WebElement search = driver.findElement(By.name(ename));
-		search.clear();
-		search.sendKeys(PATIENT_NAME);
+        // Enter the name
+        String ename = "searchName";
+        wait.until( ExpectedConditions.visibilityOfElementLocated( By.name( ename ) ) );
+        final WebElement search = driver.findElement( By.name( ename ) );
+        search.clear();
+        search.sendKeys( PATIENT_NAME );
 
-		ename = "submit";
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(ename)));
-		final WebElement searchButton = driver.findElement(By.name(ename));
-		searchButton.click();
-	}
+        ename = "submit";
+        wait.until( ExpectedConditions.visibilityOfElementLocated( By.name( ename ) ) );
+        final WebElement searchButton = driver.findElement( By.name( ename ) );
+        searchButton.click();
+    }
 
-	@Then("Then the patients medical information is displayed")
-	public void checkRecords() {
+    @Then ( "the patients medical information is displayed" )
+    public void checkRecords () {
 
-		String[] expected = { PATIENT_NAME, PATIENT_DOB, PATIENT_GENDER, PATIENT_BLOOD };
+        final String[] expected = { PATIENT_NAME, PATIENT_DOB, PATIENT_GENDER, PATIENT_BLOOD };
 
-		for (int i = 0; i < expected.length; i++) {
-			assertTrue(driver.getPageSource().contains(expected[i]));
-		}
-	}
+        for ( int i = 0; i < expected.length; i++ ) {
+            assertTrue( driver.getPageSource().contains( expected[i] ) );
+        }
+    }
 }
--- iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/EmergencyRecords.feature
+++ iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/EmergencyRecords.feature
@@ -6,7 +6,7 @@ Feature: As a developer, I want to automate
 		 be confirmed through the UI 
 		 
 Scenario Outline: HCP views emergency health records
-Given There is a patient with the name <MID>
+Given There is a patient with the name: <MID>
 And I log into iTrust2 as an HCP
 And I navigate to the Emergency Health Records Page
 When I fill in the username of the patient
@@ -16,7 +16,7 @@ Examples:
 	|AliceThirteen|
 
 Scenario Outline: ER views emergency health records
-Given There is a patient with the name <MID>
+Given There is a patient with the name: <MID>
 And I log into iTrust2 as an ER
 And I navigate to the Emergency Health Records Page
 When I fill in the username of the patient
